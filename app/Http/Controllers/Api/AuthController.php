3c94700af5aabff8ba4dc74d7fc11112013f34ce
baboutirera <baboutirera@gmail.com>
2022-03-17 15:17:52 +0100

    setup laravel passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+
+    }
+
+    public function register(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required | string | max:255',
+            'email' => 'required | string | max:255 | email | unique:users',
+            'password' => 'required | between:8, 255 | confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (!$user) {
+            return response()->json(["success" => false, "message" => "Echec de l'enregistrement"], 500);
+        }
+
+        return response()->json(["errors" => true, "message" => "Inscription réussie"], 200);
+
+    }
+
+
+}
