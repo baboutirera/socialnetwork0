e0bed6b1720b0066c83e83ed2551bde79beca7f1
baboutirera <baboutirera@gmail.com>
2022-03-18 08:52:20 +0100

    setup Vuex

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,12 +1,36 @@
 import { createRouter , createWebHistory } from "vue-router";
 import Container from "../views/layout/Container.vue";
+import Register from "../views/Auth/Register.vue";
+import Login from "../views/Auth/Login.vue";
+import Dashboard from "../views/pages/Dashboard.vue";
+import UserProfile from "../views/pages/UserProfile.vue";
 
 const routes = [
     {
         path: "/",
         name: Container,
         component: Container
-    }
+    },
+    {
+        path: "/login",
+        name: Login,
+        component: Login
+    },
+    {
+        path: "/register",
+        name: Register,
+        component: Register
+    },
+    {
+        path: "/dashboard",
+        name: Dashboard,
+        component: Dashboard
+    },
+    {
+        path: "/userprofile",
+        name: UserProfile,
+        component: UserProfile
+    },
 ]
 
 const router = createRouter({
